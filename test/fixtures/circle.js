077791d883c444d07141e26ba9e33882704d6a36
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-02-27 21:19:20 +0100

    Write a first real test

---
+++ test/fixtures/circle.js
@@ -0,0 +1,5 @@
+export default `
+<svg>
+  <circle/>
+</svg>
+`;
--- test/parametric-svg.js
+++ test/parametric-svg.js
@@ -1,33 +1,55 @@
 import tape from "tape-catch";
 
+import toDOM from "domify";
+import parametricSVG from "../source/parametric-svg";
+
+import circle from "./fixtures/circle";
+
 function test (title, ...rest) {
   return tape("parametricSVG:  " + title, ...rest);
   }
 
 
 test("Works with an SVG document root", (is) => {
+  let svg = toDOM(circle);
+  parametricSVG(svg);
+
+  is.ok
+    ( svg instanceof SVGSVGElement
+    , "leaving the SVG root intact"
+    );
+  is.equal
+    ( svg.getElementById("circle-r-6").getAttribute("r")
+    , "6"
+    );
   is.end();
   });
+
 
 test("Works with an SVG element", (is) => {
   is.end();
   });
 
+
 test("Works with a VirtualTree", (is) => {
   is.end();
   });
+
 
 test("Changes nothing when no attribute is parametric", (is) => {
   is.end();
   });
 
+
 test("Creates a new attribute", (is) => {
   is.end();
   });
 
+
 test("Updates an existing attribute", (is) => {
   is.end();
   });
+
 
 test("Changes multiple attributes at once", (is) => {
   is.end();
