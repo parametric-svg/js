1ce613344e9a1c19613ff8df53990fa9c20bff51
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-02-27 21:35:13 +0100

    Extend fixture

--- test/fixtures/circle.js
+++ test/fixtures/circle.js
@@ -1,5 +1,11 @@
 export default `
-<svg>
-  <circle/>
+<svg xmlns:parametric="https://github.com/parametric-svg/spec/tree/0.1">
+  <defs>
+    <parametric:ref param="factor" default="2" />
+  </defs>
+  <circle id="circle-r-6"
+    r="3"
+    parametric:r="3 * factor"
+    />
 </svg>
 `;
