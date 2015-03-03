c23058a156f82835d62a44327ce3e6f63d3455cd
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-03-04 00:23:43 +0100

    Stub parametricSVG(svgRoot)

--- source/parametric-svg.js
+++ source/parametric-svg.js
@@ -1,43 +1,56 @@
- /**
-  * @function parametricSVG
-  *   Parse and render all elements within the `<svg>` `root` node. Parameters declared with
-  *   `<param>` elements will be used for calculation, unless you override them with `parameters`.
-  *
-  * @param  {SVGSVGElement}  root
-  *
-  * @param  {Object}  [parameters]
-  *   A hash of additional parameters. They'll extend and override those declared with `<param>`
-  *   elements.
-  *
-  * @returns {VirtualTree}
-  *   A cached virtual DOM tree for lightning-fast redraws.
-  */
+import virtualTree from "./virtual-tree";
+import getParameters from "./utils/get-parameters";
 
- /**
-  * @function parametricSVG
-  *   Pass any SVG node (like `<circle>`) to parse and render the node and all its descendants.
-  *
-  * @param  {SVGElement}  element
-  *
-  * @param  {Object}  parameters
-  *   A hash of parameters.
-  *
-  * @returns {VirtualTree}
-  *   A cached virtual DOM tree for lightning-fast redraws.
-  */
 
- /**
-  * @function parametricSVG
-  *   Pass a cached `VirtualTree` to render the tree without reparsing any DOM. This is the fastest
-  *   option.
-  *
-  * @param  {VirtualTree}  virtualTree
-  *
-  * @param  {Object}  [parameters]
-  *   Supply a hash of additional parameters. They'll extend and replace cached ones.
-  *
-  * @returns {VirtualTree}
-  *   A cached virtual DOM tree for lightning-fast redraws.
-  */
+export default function parametricSVG (root, parameters={}) {
+  let tree, svgRoot;
 
-export default function parametricSVG () {}
+   /**
+    * @function parametricSVG
+    *   Parse and render all elements within the `svgRoot`. Defaults set with `<ref>` elements
+    *   will be used for calculation, unless you override them with `parameters`.
+    *
+    * @param  {SVGSVGElement}  svgRoot
+    *   An `<svg>` element.
+    *
+    * @param  {Object}  [parameters]
+    *   A hash of additional parameters. They'll extend and override defaults set with `<ref>`
+    *   elements.
+    *
+    * @returns {VirtualTree}
+    *   A cached virtual DOM tree for lightning-fast redraws.
+    */
+  if ((svgRoot = root) instanceof SVGSVGElement) {
+    tree = new virtualTree
+      ( Array.from(svgRoot.childNodes)
+      , getParameters(svgRoot)
+      );
+
+
+   /**
+    * @function parametricSVG
+    *   Pass any SVG node (like `<circle>`) to parse and render the node and all its descendants.
+    *
+    * @param  {SVGElement}  element
+    *
+    * @param  {Object}  parameters
+    *   A hash of parameters.
+    *
+    * @returns {VirtualTree}
+    *   A cached virtual DOM tree for lightning-fast redraws.
+    */
+
+   /**
+    * @function parametricSVG
+    *   Pass a cached `VirtualTree` to render the tree without reparsing any DOM. This is the fastest
+    *   option.
+    *
+    * @param  {VirtualTree}  virtualTree
+    *
+    * @param  {Object}  [parameters]
+    *   Supply a hash of additional parameters. They'll extend and replace cached ones.
+    *
+    * @returns {VirtualTree}
+    *   A cached virtual DOM tree for lightning-fast redraws.
+    */
+  }
