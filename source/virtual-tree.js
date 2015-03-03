ebd5b830e2a7d168f3f9f1ef5b55a9b68a107400
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-03-04 00:25:12 +0100

    First draft of VirtualTree

--- source/virtual-tree.js
+++ source/virtual-tree.js
@@ -3,11 +3,21 @@
  */
 
 export default class VirtualTree
-  { constructor(initial) {
+  { constructor(entryPoints, initial) {
+    let self = this;
+
+    // Extend the instance's properties with `initial`.
     Object.assign(this
       , { parameters: {}
+        , elements: []
         }
       , initial
       );
+
+    // Populate `this.elements`.
+    entryPoints.forEach(function recurse (element) {
+      self.elements.push(element);
+      Array.from(element.childNodes).forEach(recurse);
+      });
     }
   }
