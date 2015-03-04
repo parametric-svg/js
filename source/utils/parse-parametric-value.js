151dc1a0f19f2b6ebe098e5429a076e92bfe2761
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-03-04 23:24:04 +0100

    Add prototype parseParametricValue

---
+++ source/utils/parse-parametric-value.js
@@ -0,0 +1,39 @@
+import acorn from "acorn";
+
+
+export default function parseParametricValue (value) {
+  // Parse the value into an AST.
+  let ast;
+  try {ast = acorn.parse(value);}
+  catch (error) {return {error};}
+
+  // Error if the value is not exactly one expression.
+  if (ast.type != "Program" || !ast.body) return {error: new SyntaxError(
+    "Unknown format of parametric attribute."
+    )};
+  if (ast.body.length != 1) return {error: new SyntaxError(
+    "A parametric attribute must be exactly one ES expression."
+    )};
+
+  // Find parameter names.
+  let parameterNames = [];
+  (function recurse (branch) {
+    if (branch.type == "Identifier") {
+      parameterNames.push(branch.name);
+      return;
+      }
+
+    let twig;
+    for (let key in branch) if (branch.hasOwnProperty(key)) {
+      if ((twig = branch[key]) && typeof twig == "object") recurse(twig);
+      }
+    })(ast.body[0]);
+
+  // Parse the `value` as a function – error if that fails.
+  let func;
+  try {func = Function.apply(parameterNames.concat(value));}
+  catch (error) {return {error};}
+
+  // Return the function and parameter names.
+  return {parameterNames, func};
+  }
