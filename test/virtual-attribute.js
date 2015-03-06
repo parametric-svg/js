9893f46edfbab7c1f65dace3888c39090ce27737
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-03-06 19:00:10 +0100

    Write tests for VirtualAttribute

--- test/virtual-attribute.js
+++ test/virtual-attribute.js
@@ -9,16 +9,75 @@ import circles from "./_/fixtures/circles";
 import VirtualAttribute from "../source/virtual-attribute";
 
 
-test("Just curious what the results are", (is) => {
-  let svg = toDOM(circles);
+let svg = toDOM(circles);
 
-  var circle = svg.getElementById("circle-simple");
-  var attributes = asObject(
-    Array.from(circle.attributes).map(attribute => (
+let circle = svg.getElementById("circle-factor-plus");
+let attributes = Array.from(circle.attributes);
+let vAttributeList = attributes.map(attribute => (
+  new VirtualAttribute(attribute, circle)
+  ));
+let vAttributes = asObject(
+  attributes.map((attribute, index) => (
     { key: attribute.name
-    , value: new VirtualAttribute(attribute, circle)
-    })));
-  console.log(attributes);
+    , value: vAttributeList[index]
+    }))
+  );
+
+
+test("Returns what it should return", (is) => {
+
+  is.equal
+    ( vAttributeList.filter(vAttribute => (
+      (  vAttribute instanceof VirtualAttribute
+      && !vAttribute.error
+      )))
+      .length
+    , 2
+    , "returning a VirtualAttribute for every parametric attribute"
+    );
+
+  is.equal
+    ( vAttributeList.filter(vAttribute => vAttribute.error)
+      .length
+    , vAttributeList.length - 1
+    , "erroring otherwise"
+    );
 
   is.end();
+  });
+
+
+test("Does what it should do", (is) => {
+  let parametricR = vAttributes["parametric:r"];
+  let parametricFill = vAttributes["parametric:fill"];
+
+  is.equal
+    ( parametricR.counterpart.name
+    , "r"
+    , "finding its pair static attribute"
+    );
+
+  is.equal
+    ( parametricFill.counterpart
+    , null
+    , "…or saying `null` otherwise"
+    );
+
+  is.deepEqual
+    ( parametricR.parameterNames
+    , ["factor", "plus"]
+    , "spotting parameters it depends on"
+    );
+
+  is.equal
+    ( parametricFill.func("green")
+    , "green"
+    , "with a simple attribute replacement"
+    );
+
+  is.equal
+    ( parametricR.func(3, 2)
+    , 3 * 3 + 2
+    , "with an attribute equation"
+    );
   });
