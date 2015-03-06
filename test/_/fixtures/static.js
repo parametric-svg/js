85e68d4446420787f38e1271bc1663930b9cb727
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-03-06 23:21:46 +0100

    Test behavior with static SVG

---
+++ test/_/fixtures/static.js
@@ -0,0 +1,9 @@
+export default
+`<svg version="1.1"
+  >
+  <circle id="circle-static"
+    r="20"
+    x="0"
+    />
+  </svg>
+`;
--- test/parametric-svg/behavior.js
+++ test/parametric-svg/behavior.js
@@ -1,5 +1,29 @@
+import toDOM from "domify";
+
 import _test from "../_/test";
 let test = _test("parametric-svg behavior");
+
+import staticFixture from "../_/fixtures/static";
+
+import parametricSVG from "../../source/parametric-svg";
+
+
+test("Changes nothing when no attribute is parametric", (is) => {
+  let svg = toDOM(staticFixture);
+  let snapshot = svg.outerHTML;
+  parametricSVG(svg);
+
+  let container = document.createElement("div");
+  container.appendChild(svg);
+
+  is.equal
+    ( svg.outerHTML
+    , snapshot
+    , "you bet!"
+    );
+
+  is.end();
+  });
 
 
 test.skip("Replaces simple values", (is) => {
